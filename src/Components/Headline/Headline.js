e637a3791842e239aef9a529f45c5b36a9c9c237
Shahier Nashaat <shahiernashaat97@gmail.com>
2023-01-22 00:16:28 +0200

    fix the underline title

--- src/Components/Headline/Headline.js
+++ src/Components/Headline/Headline.js
@@ -46,8 +46,6 @@ function Headline() {
           Look through some of my work and experience! If you
           like what you see and have a project you need to be
           coded, don’t hesitate to contact me.
-          I can help you build a product,
-          feature or website Look through.
         </p>
       </div>
       <div className="icons">
