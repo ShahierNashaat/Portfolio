62bbd3d0a9c7a15a65b78964609594264c82d9ee
Shahier Nashaat <shahiernashaat97@gmail.com>
2023-04-19 01:38:13 +0200

    Add work projects section

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,4 +1,5 @@
 import './Work.css';
+import projects from '../../data/projects.json';
 
 function Work() {
   return (
@@ -8,6 +9,29 @@ function Work() {
         <span className="underline">
           <span className="animation" />
         </span>
+      </div>
+
+      <div className="work-cards">
+        {projects.map((project) => (
+          <div className="work-card" key={project.name}>
+            <div className="project-image">
+              <img src={project.featuredImage} alt="project" />
+              <div className="live-source-icons">
+                {project.linkToLive != null
+                && (
+                <a className="live-button" href={project.linkToLive} target="_blank" rel="noreferrer">
+                  <i className="fas fa-circle-notch" />
+                </a>
+                )}
+                <a className="source-button" href={project.linkToSource} target="_blank" rel="noreferrer">
+                  <i className="fab fa-github" />
+                </a>
+              </div>
+            </div>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
